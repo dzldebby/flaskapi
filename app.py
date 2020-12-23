6a665fb1b68ca4ac50e32bc754c7924e811cc0a3
dzldebby <debby.lingzl@gmail.com>
2020-12-23 15:48:34 +0800

    initial commit"

---
+++ app.py
@@ -0,0 +1,40 @@
+# app.py
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import joblib
+import numpy as np
+
+APP = Flask(__name__)
+API = Api(APP)
+
+IRIS_MODEL = joblib.load('iris.mdl')
+
+
+class Predict(Resource):
+
+    @staticmethod
+    def post():
+        parser = reqparse.RequestParser()
+        parser.add_argument('petal_length')
+        parser.add_argument('petal_width')
+        parser.add_argument('sepal_length')
+        parser.add_argument('sepal_width')
+
+        args = parser.parse_args()  # creates dict
+
+        X_new = np.fromiter(args.values(), dtype=float)  # convert input to array
+
+        out = {'Prediction': IRIS_MODEL.predict([X_new])[0]}
+
+        return out, 200
+
+
+API.add_resource(Predict, '/predict')
+
+@APP.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+
+if __name__ == '__main__':
+    APP.run(debug=True, port='1080')
